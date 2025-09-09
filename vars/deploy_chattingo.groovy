a7509f1cb0201e715168b5272368e97a527e0c82
Viraj Dalave <virajdvd7@gmail.com>
2025-09-09 16:32:30 +0530

    Update deploy_chattingo.groovy

--- vars/deploy_chattingo.groovy
+++ vars/deploy_chattingo.groovy
@@ -1,9 +1,11 @@
 def call(){
   withCredentials([string(credentialsId: 'DB_PASS', variable: 'DB_PASS'), string(credentialsId: 'JWT_SECRET', variable: 'JWT_SECRET'), string(credentialsId: 'SPRING_DATASOURCE_URL', variable: 'SPRING_DATASOURCE_URL'), string(credentialsId: 'SPRING_DATASOURCE_USERNAME', variable: 'SPRING_DATASOURCE_USERNAME'), usernamePassword('credentialsId':"docker-creds",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
-      sh "docker login  -u ${dockerHubUser} -p ${dockerHubPass}" 
+      
+    sh '''
+      docker login  -u ${dockerHubUser} -p ${dockerHubPass}
+      docker compose -f ./chattingo/docker-compose.prod.yml down || true
+      docker compose -f ./chattingo/docker-compose.prod.yml up -d 
+    '''
   }
-  sh '''
-    docker compose -f ./chattingo/docker-compose.prod.yml down || true
-    docker compose -f ./chattingo/docker-compose.prod.yml up -d 
-  '''
+  
 }
